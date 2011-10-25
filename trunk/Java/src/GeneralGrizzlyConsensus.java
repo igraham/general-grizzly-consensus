865091d7ce5f253b8e2725fc27c4e85d7b1a8732
megamanexpert@gmail.com <megamanexpert@gmail.com@53328dc4-0b30-a122-97ca-59b2398e10a6>
2011-10-25 14:59:08 +0000

    Transfered code from Google docs to the this repository.

---
+++ trunk/Java/src/GeneralGrizzlyConsensus.java
@@ -0,0 +1,21 @@
+package GeneralGrizzlyConsensus;
+
+/**
+* @author Stephen Kent
+*
+*         This is intended to be the main executable class for the clicker.
+*
+*/
+public class GeneralGrizzlyConsensus
+{
+
+   /**
+    * @param args
+    */
+   public static void main(String[] args)
+   {
+       GGCChooseGUI chooser = new GGCChooseGUI();
+       chooser.pack();
+       chooser.setVisible(true);
+   }
+}
